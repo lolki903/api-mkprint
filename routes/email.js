d4ea556df14fcea5af8eeb5991aa4354c020dfbe
julesgaborit <jules.gaborit@my-digital-school.org>
2023-10-14 18:37:40 +0200

    add swagger

--- routes/email.js
+++ routes/email.js
@@ -4,10 +4,34 @@ const router = express.Router();
 const { validate } = require("../controllers/email.js");
 const { verifyToken } = require("../middleware/jwttoken.js");
 
-// router.get("/get", verifyToken,getuser);
+/**
+ * @swagger
+ * /email/validate:
+ *   post:
+ *     summary: Validate email
+ *     description: Validate email with verification code
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *               code:
+ *                 type: string
+ *             example:
+ *               email: john@example.com
+ *               code: 123456
+ *     responses:
+ *       200:
+ *         description: Email verified successfully
+ *       404:
+ *         description: Invalid verification code
+ *       500:
+ *         description: Error in email verification
+ */
 router.post("/validate" , validate);
-// router.post("/login" ,verifyToken ,login);
-// router.post("/create" , createNewsletter);
 
 module.exports = router;
-// export default router;
--- routes/message.js
+++ routes/message.js
@@ -2,11 +2,47 @@ const express = require("express");
 const { createmessage, getmessage } = require("../controllers/message");
 const router = express.Router();
 
-// router.get("/get", verifyToken,getuser);
+/**
+ * @swagger
+ * /message/get:
+ *   get:
+ *     summary: Get all messages
+ *     description: Retrieve a list of all messages
+ *     responses:
+ *       200:
+ *         description: A list of messages.
+ */
 router.get("/get",getmessage)
+
+/**
+ * @swagger
+ * /message/create:
+ *   post:
+ *     summary: Create a new message
+ *     description: Create a new message with the provided information
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               firstname:
+ *                 type: string
+ *               lastname:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               message:
+ *                 type: string
+ *     responses:
+ *       206:
+ *         description: Message created and verification email sent
+ *       203:
+ *         description: Message created
+ *       409:
+ *         description: Error creating message
+ */
 router.post("/create" , createmessage);
-// router.post("/login" ,verifyToken ,login);
-// router.post("/create" , createNewsletter);
 
 module.exports = router;
-// export default router;
